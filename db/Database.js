28550b4b696cc53c3cff53207b2d2853ad685d62
shubham.ghogle <shubham.ghogle@innovyatech.com>
2025-08-19 14:43:56 +0530

    prodcut add

--- db/Database.js
+++ db/Database.js
@@ -1,13 +1,17 @@
 const mongoose = require("mongoose");
 
+// const url = process.env.DB_URL
+
+const url = "mongodb://127.0.0.1:27017/sema_local"
+
 const connectDatabase = () => {
   mongoose
-    .connect(process.env.DB_URL, {
+    .connect(url, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
-    .then((data) => {
-      console.log(`mongod connected with server: ${process.env.DB_URL}`);
+    .then(() => {
+      console.log(`mongod connected with server: ${url}`);
     });
 };
 
