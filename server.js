660179bd4ea49be2e89ac63ce76073b3b8359ff2
shubham.ghogle <shubham.ghogle@innovyatech.com>
2025-11-04 14:36:16 +0530

    cors

--- server.js
+++ server.js
@@ -40,6 +40,10 @@ const allowedOriginsCors = [
   "http://localhost:5173",
   "http://test.semamart.com",
   "https://test.semamart.com",
+  "https://live.semamart.com",
+  "http://live.semamart.com",
+  "https://www.semamart.com",
+  "http://www.semamart.com",
 ]
 
 app.use(
